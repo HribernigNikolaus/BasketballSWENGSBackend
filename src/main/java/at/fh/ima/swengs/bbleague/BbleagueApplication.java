8ab52da869385f17648140bd08386a8b2fa7e052
x-qlusive <patrick.plank@edu.fh-joanneum.at>
2017-12-29 12:37:06 +0100

    Cannot determine embedded database driver class for database type NONE error.

--- src/main/java/at/fh/ima/swengs/bbleague/BbleagueApplication.java
+++ src/main/java/at/fh/ima/swengs/bbleague/BbleagueApplication.java
@@ -1,9 +1,13 @@
 package at.fh.ima.swengs.bbleague;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 public class BbleagueApplication {
 
 	public static void main(String[] args) {
